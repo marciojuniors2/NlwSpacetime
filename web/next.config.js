3a0e7c106c9d02c7d2e4afba2abb064922cb86b7
marcio junior <marcio.junior@bc9.com.br>
2023-05-22 19:35:11 -0300

    finished project

---
+++ web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', 'localhost', '192.168.10.3'],
+  },
+}
+
+module.exports = nextConfig
---
+++ web/src/lib/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: 'http://192.168.10.5:3333',
+})
